7a8830e7213c173166acd30946ddfb3a453958a6
Ben Thomasson <ben.thomasson@gmail.com>
2016-10-08 17:14:58 -0400

    Adding method to install multi_nextval function

--- django_postgres_utils/util.py
+++ django_postgres_utils/util.py
@@ -23,11 +23,43 @@ def make_materialized_view_non_unique(name, qs):
     return sql
 
 
+multi_nextval = '''
+CREATE OR REPLACE FUNCTION multi_nextval(use_seqname TEXT, use_increment INT4) RETURNS INT4 AS $$
+
+DECLARE
+
+    reply int4;
+
+BEGIN
+
+    perform pg_advisory_lock(123);
+
+    execute 'ALTER SEQUENCE ' || quote_ident(use_seqname) || ' INCREMENT BY ' || use_increment::text;
+
+    reply := nextval(use_seqname);
+
+    execute 'ALTER SEQUENCE ' || quote_ident(use_seqname) || ' INCREMENT BY 1';
+
+    perform pg_advisory_unlock(123);
+
+    return reply;
+
+END;
+
+$$ LANGUAGE 'plpgsql';
+'''
+
+
+def install_multi_nextval():
+    cursor = connection.cursor()
+    cursor.execute(multi_nextval)
+
+
 def model_pk_seq(model, n):
     if n <= 0:
         return xrange(0)
     cursor = connection.cursor()
-    if not hasattr(model._meta):
+    if not hasattr(model._meta, 'pk_sequence'):
         raise AttributeError("pk_sequence is required on {0}._meta".format(model))
     cursor.execute("select multi_nextval(%s, %s)", (model._meta.pk_sequence, n))
     result = cursor.fetchone()[0]
